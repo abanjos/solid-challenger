42e3a64b83da863c0e11103a1fee82e913e7bd26
abanjos <andrebrandg@gmail.com>
2022-07-12 19:53:45 -0300

    done

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,7 +11,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userExist = this.usersRepository.findByEmail(email);
+
+    if(userExist) {
+        throw new Error("User already exist!!")
+    }
+
+    return this.usersRepository.create({ email, name })
+
+
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +10,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+   const userAdmin = this.usersRepository.findById(user_id);
+
+   if(!userAdmin) {
+        throw new Error("User is not exist!")
+   }
+
+   if(!userAdmin.admin) {
+        throw new Error("User is not admin!")
+   }
+
+   return this.usersRepository.list();
+   
   }
 }
 
